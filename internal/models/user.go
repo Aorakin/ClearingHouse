72704f88b578aea2c201e93ec9ab39c60f5026a4
boomoioi <n.nanthawisit@gmail.com>
2025-01-26 22:44:45 +0700

    feat: update Docker configuration and refactor user model to use UUIDs

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,11 +1,9 @@
 package models
 
 import (
-	"errors"
 	"time"
 
 	"github.com/google/uuid"
-	"gorm.io/gorm"
 )
 
 type UserRole string
@@ -15,41 +13,16 @@ const (
 	UserRoleAdmin UserRole = "admin"
 )
 
+type BaseModel struct {
+	ID        uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key" json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
 type User struct {
-	ID        string `gorm:"type:char(36);primary_key;not null"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	DeletedAt *time.Time `sql:"index"`
-	Username  string     `gorm:"size:255;not null;uniqueIndex" json:"username" validate:"required,min=2,max=30"`
-	Email     string     `json:"email" validate:"required,email"`
-	Password  string     `gorm:"not null" json:"password" validate:"required,min=8"`
-	Role      UserRole   `gorm:"type:varchar(50);default:'user'" json:"role"`
-}
-
-func (user *User) BeforeCreate(tx *gorm.DB) (err error) {
-	if user.ID == "" {
-		user.ID = uuid.New().String()
-	}
-	return
-}
-
-func (u *User) BeforeUpdate(tx *gorm.DB) (err error) {
-	var existingUser User
-	if err := tx.Unscoped().Where("id = ?", u.ID).First(&existingUser).Error; err != nil {
-		return err
-	}
-
-	if u.Role != existingUser.Role {
-		return errors.New("role cannot be changed")
-	}
-
-	return nil
-}
-
-func (r UserRole) IsValid() bool {
-	switch r {
-	case UserRoleUser, UserRoleAdmin:
-		return true
-	}
-	return false
+	BaseModel
+	Username string   `gorm:"not null;uniqueIndex" json:"username" validate:"required,min=2,max=30"`
+	Email    string   `gorm:"not null;unique" json:"email" validate:"required,email"`
+	Password string   `gorm:"not null" json:"password" validate:"required,min=8"`
+	Role     UserRole `gorm:"type:varchar(50);default:'user'" json:"role"`
 }
