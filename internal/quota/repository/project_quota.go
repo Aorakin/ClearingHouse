6be97b9dca319ddd4ed44b9439517b1c7ec2f913
Vorachot Banluepongpan <vorachot.ban@gmail.com>
2026-01-09 00:33:29 +0700

    feat: enhance GetProjectQuotaByProjectID to preload Resource and ResourceType for improved data retrieval

--- internal/quota/repository/project_quota.go
+++ internal/quota/repository/project_quota.go
@@ -11,7 +11,7 @@ func (r *QuotaRepository) CreateProjectQuota(projectQuota *models.ProjectQuota) 
 
 func (r *QuotaRepository) GetProjectQuotaByProjectID(projectID uuid.UUID) ([]models.ProjectQuota, error) {
 	var projectQuotas []models.ProjectQuota
-	err := r.db.Debug().Preload("Resources.ResourceProp").Where("project_id = ?", projectID).
+	err := r.db.Debug().Preload("Resources.ResourceProp.Resource").Preload("Resources.ResourceProp.Resource.ResourceType").Where("project_id = ?", projectID).
 		Find(&projectQuotas).Error
 	if err != nil {
 		return nil, err
